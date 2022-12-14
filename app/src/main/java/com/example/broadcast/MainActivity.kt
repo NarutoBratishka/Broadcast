9011bb2bf970dd192e485bbd70c2daf46d0da590
NarutoBratishka <katorabian@gmail.com>
2022-12-15 02:32:01 +0700

    create: simple BroadcastReceiver

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.broadcast
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val receiver = MyReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        registerReceiver(receiver, IntentFilter(Intent.ACTION_BATTERY_LOW))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
